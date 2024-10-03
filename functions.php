4f8bb4699cffd9843030aa6e765d560208391ef9
Zayna <zk10-91@hotmail.com>
2024-10-03 14:27:51 +0200

    Charger jquery

--- functions.php
+++ functions.php
@@ -7,15 +7,26 @@ add_action( 'wp_enqueue_scripts', 'nathaliemota_enqueue_styles' );
 
 // Charger les scripts JS du thème
 function nathaliemota_enqueue_scripts() {
-    wp_enqueue_script( 'nathaliemota-modal-script', get_template_directory_uri() . '/js/scripts.js', array(), null, true );
+    // Charger jQuery
+    wp_enqueue_script('jquery');
+
+    // Charger le script personnalisé
+    wp_enqueue_script( 'nathaliemota-modal-script', get_template_directory_uri() . '/assets/js/scripts.js',  array('jquery'), filemtime(get_stylesheet_directory() . '/assets/js/scripts.js'), true );
 }
 add_action( 'wp_enqueue_scripts', 'nathaliemota_enqueue_scripts' );
 
-// Support pour les Menus
-function nathaliemota_setup() {
+// Fonction pour enregistrer les emplacements de menus
+function nathaliemota_register_menus() {
     register_nav_menus(array(
-        'main-menu' => __('Main Menu', 'nathaliemota'),
+        'primary-menu' => __('Menu Principal'),
+        'footer-menu' => __('Menu du Pied de Page')
     ));
 }
-add_action('after_setup_theme', 'nathaliemota_setup');
+add_action('after_setup_theme', 'nathaliemota_register_menus');
+
+
+
+
+
 ?>
+
