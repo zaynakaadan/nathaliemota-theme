61dff9e8c83d3b1f146b4bd2156b6ca25b0f29f1
Zayna <zk10-91@hotmail.com>
2024-10-03 14:21:55 +0200

    Mise à jour du header : ajout des menus desktop et mobile

--- header.php
+++ header.php
@@ -3,20 +3,38 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
+     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-    <header>
-        <div class="logo">
-            <img src="<?php echo get_template_directory_uri(); ?>/path/to/logo.png" alt="Logo Nathalie Mota">
-        </div>
-        <nav>
-            <?php
-            wp_nav_menu(array(
-                'theme_location' => 'main-menu',
-                'menu_class' => 'nav-menu',
-            ));
-            ?>
-        </nav>
-    </header>
+<?php wp_body_open(); ?>	
+	<header id="header" class="header flexrow">
+		<div class="container-header flexrow">
+			<a href="<?php echo home_url( '/' ); ?>" aria-label="Page d'accueil de Nathalie Mota">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo_nathalie_mota.png" 
+				alt="Logo <?php echo bloginfo('name'); ?>">
+			</a>
+			<nav id="navigation">
+				<?php 
+					// Affichage du menu main déclaré dans functions.php
+					wp_nav_menu(array('theme_location' => 'primary-menu')); 
+				?>
+				<button id="modal__burger" class="btn-modal" aria-label="Menu pour la version portable">
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                </button>
+				
+                <div id="modal__content" class="modal__content">           
+					<?php 				
+					wp_nav_menu(array('theme_location' => 'primary-menu')); 
+					?>
+                </div>
+			</nav>
+		</div>
+	</header>	
