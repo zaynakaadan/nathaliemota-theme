5cc5286423bea38ce48049d065f21ddb803a0118
Zayna <zk10-91@hotmail.com>
2024-11-29 00:16:30 +0100

    Gestion de la fermeture et de l'ouverture du menu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,19 +3,20 @@ jQuery(document).ready(function($) {
 
       // Rechercher les éléments
       const contactBtn = $(".contact");
-      const popupOverlay = $(".popup-overlay");
+      const popupOverlay = $(".popup-overlay");      
 
       //console.log("Nombre de boutons contact trouvés : ", contactBtn.length);
       //console.log("Popup overlay disponible : ", popupOverlay.length > 0);
 
+      // Vérification que les éléments existent
       if (contactBtn.length > 0 && popupOverlay.length > 0) {
-          // Ouvrir la modale au clic sur les boutons de contact
+          // Ouvrir la modale au clic sur les boutons de contact 
           contactBtn.on("click", function(event) {
               event.preventDefault(); // Empêche le comportement par défaut du lien
               console.log("Bouton contact cliqué !");
-              popupOverlay.removeClass("hidden");
-          });
-
+              // Afficher la popup
+              popupOverlay.removeClass("hidden");       
+       });
           // Fermeture de la popup contact au clic sur la zone grise
           popupOverlay.on("click", function(e) {
               if ($(e.target).is(popupOverlay)) { // Vérifie si c'est bien la zone grise
@@ -26,8 +27,6 @@ jQuery(document).ready(function($) {
       } else {
           console.error("Les éléments .contact ou .popup-overlay ne sont pas disponibles !");
       }
-
-
 
     (function ($) {
         $(document).ready(function () {
@@ -41,16 +40,14 @@ jQuery(document).ready(function($) {
           // Fermeture du menu lorsqu'on clique sur un lien
           $("a").click(function () {
             
-            if ($(".modal__content").hasClass("open")) {
-              setTimeout(function() {
-                $(".modal__content").removeClass("animate-modal");
-                $(".modal__content").removeClass("open");
-                $(".btn-modal").removeClass("close");
-                }, 200); // 200ms pour correspondre à l'animation
-            }
+          if ($(".modal__content").hasClass("open")) {
+            setTimeout(function() {
+              $(".modal__content").removeClass("animate-modal");
+              $(".modal__content").removeClass("open");
+              $(".btn-modal").removeClass("close");
+            }, 200); // 200ms pour correspondre à l'animation
+          }
           });
         });
       })(jQuery);
-
-
 });
