d77d79fc71347785af19229743d33276a0dccf5a
Zayna <zk10-91@hotmail.com>
2024-11-29 00:20:47 +0100

    Ajout fichier single.php

--- single.php
+++ single.php
@@ -1,10 +1,53 @@
 <?php
-get_header();
-if ( have_posts() ) {
-    while ( have_posts() ) {
-        the_post();
-        get_template_part( 'template-parts/content', 'single' );
-    }
-}
-get_footer();
+/**
+ * The single : ATRICLE BLOG 
+ *
+ * @package WordPress
+ * @subpackage nathaliemota-theme
+ */
+
+	get_header();
 ?>
+
+<div id="wrap">
+    <section id="content">
+		<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+			
+		<article class="post">
+			<?php 
+				// permet d’afficher l’image mise en avant
+				the_post_thumbnail(); 
+			?>
+
+			<h1><?php the_title(); ?></h1>
+
+			<div class="post__meta">
+				<?php echo get_avatar( get_the_author_meta( 'ID' ), 40 ); ?>
+				<p>
+				Publié le <?php the_date(); ?>
+				par <?php the_author(); ?>
+				Dans la catégorie <?php the_category(); ?>
+				Avec les étiquettes <?php the_tags(); ?>
+				</p>
+			</div>
+
+			<div class="post__content">
+				<?php the_content(); ?>
+			</div>
+		</article>
+		<?php endwhile; endif; ?>
+
+		<p>Navigation</p>
+		<div class="site__navigation flexrow">
+			<div class="site__navigation__prev">
+				<?php previous_post_link( 'Article Précédent<br>%link' ); ?>
+			</div>
+			<div class="site__navigation__next">
+				<?php next_post_link( 'Article Suivant<br>%link' ); ?> 
+			</div>
+		</div>
+    </section>
+</div>
+<br>
+<?php get_footer();?>
+
